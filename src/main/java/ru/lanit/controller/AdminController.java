d594ecaf41663c01fc1dcf7ec2f13cf107508835
artemzubov <azubovv@ya.ru>
2021-12-08 20:43:09 +0300

    Admin add new category refactor

--- src/main/java/ru/lanit/controller/AdminController.java
+++ src/main/java/ru/lanit/controller/AdminController.java
@@ -3,10 +3,7 @@ package ru.lanit.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.lanit.model.entity.Category;
 import ru.lanit.service.category.CategoryService;
 
@@ -23,29 +20,47 @@ public class AdminController {
 
     @GetMapping
     public String adminPage() {
-        return "adminPage";
+        return "admin/adminPage";
     }
 
     @GetMapping("/category")
-    public String show(Model model) {
+    public String showAll(Model model) {
         model.addAttribute("categories", service.findAll());
-        return "adminListCategory";
+        return "admin/adminListCategory";
     }
 
     @GetMapping("/category/new")
-    public String create(Model model) {
+    public String showNewPage(Model model) {
         model.addAttribute("category", new Category());
         model.addAttribute("categoryList", service.findAllParentCategories());
-        return "adminNewCategory";
+        return "admin/adminNewCategory";
     }
 
     @PostMapping("/category/new")
-    public String update(Category category, @ModelAttribute("parentName") String parentName) {
-        if (parentName.isEmpty()) {
+    public String create(Category category, @ModelAttribute("parentName") String parentName) {
+        if (!parentName.isEmpty()) {
             category.setParentCategory(service.findByName(parentName));
         }
         service.save(category);
         return "redirect:/admin/category";
     }
+
+    @GetMapping("/category/update/{name}")
+    public String showUpdatePage(@PathVariable("name") String name, Model model) {
+        model.addAttribute("category", service.findByName(name));
+        model.addAttribute("categoryList", service.findAllParentCategories());
+        return "admin/adminUpdateCategory";
+    }
+
+    @PostMapping("/category/update")
+    public String update(Category category, @ModelAttribute("parentName") String parentName) {
+        if (!parentName.isEmpty()) {
+            category.setParentCategory(service.findByName(parentName));
+        }
+        service.save(category);
+        return "redirect:/admin/category";
+    }
+
+
 
 }
